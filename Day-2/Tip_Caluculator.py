bcd327569ced09da2a5fb55e652dab1c66bcd472
Teja Annamdevula <tejaannamdevula@gmail.com>
2023-10-03 12:02:30 +0530

    added string formatter to round of to 2 decimals

--- Day-2/Tip_Caluculator.py
+++ Day-2/Tip_Caluculator.py
@@ -4,4 +4,9 @@ percent = int(input("What percentage tip would you like to give? 10, 12, or 15? 
 total += total*percent/100
 people = int(input("How many people to split the bill? "))
 value = round(total/7,2)
+
+# sting formater  or %f formatter for rounding to 2 decimals 
+# since round(12.0,2) doesn't give 12.00
+
+value = "{:.2f}".format(value)
 print(f"Each person should pay: ${value}")
